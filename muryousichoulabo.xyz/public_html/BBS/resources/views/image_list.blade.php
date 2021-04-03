c1531d503b336192f4772cab38e4d5193517a5fc
Ruxon <wqtjdgus12@yahoo.co.jp>
2021-04-04 02:50:09 +0900

    画像アップロード完成

--- muryousichoulabo.xyz/public_html/BBS/resources/views/image_list.blade.php
+++ muryousichoulabo.xyz/public_html/BBS/resources/views/image_list.blade.php
@@ -1,12 +1,11 @@
 
-
 <a href="{{ route('upload_form') }}">Upload画面へ戻る</a>
 <hr />
 
-
 @foreach($images as $image)
 <div style="width: 18rem; float:left; margin: 16px;">
-      <img src="{{asset('storage/uploads/'.$image->file)}}" style="width:100%;"/>
+        <img src="/BBS/public/{{ Storage::url($image->file_path) }}" style="width:100%;"/>
+        
 <?php   
         $a = Storage::url($image->file_path);
         echo($a);
@@ -14,5 +13,3 @@
         <p>{{ $image->file_name }}</p>
 </div>
 @endforeach
-
-
