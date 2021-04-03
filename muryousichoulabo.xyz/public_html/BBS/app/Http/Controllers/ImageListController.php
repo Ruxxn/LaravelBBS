ca012ed74f7ce81c2d3ebd7a143720e84579bbc2
Ruxon <wqtjdgus12@yahoo.co.jp>
2021-04-04 01:19:26 +0900

    画像が表示されない所まで

--- muryousichoulabo.xyz/public_html/BBS/app/Http/Controllers/ImageListController.php
+++ muryousichoulabo.xyz/public_html/BBS/app/Http/Controllers/ImageListController.php
@@ -1,15 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\UploadImage;
+use App\UploadImage;
 use Illuminate\Http\Request;
 
 class ImageListController extends Controller
 {
-    
+    function show()
+    {
         $uploads = UploadImage::orderBy("id","desc")->get();
 
         return view("image_list",[
             "images" => $uploads
-        ]); 
+        ]);
+    }    
+
 }
--- muryousichoulabo.xyz/public_html/BBS/resources/views/image_list.blade.php
+++ muryousichoulabo.xyz/public_html/BBS/resources/views/image_list.blade.php
@@ -1,10 +1,17 @@
 
-<a href="{{ route('upload_form') }}">Upload</a>
+
+<a href="{{ route('upload_form') }}">Upload画面へ戻る</a>
 <hr />
 
 @foreach($images as $image)
 <div style="width: 18rem; float:left; margin: 16px;">
 	<img src="{{ Storage::url($image->file_path) }}" style="width:100%;"/>
+<?php
+	$a = Storage::url($image->file_path);
+	echo($a);
+?>
 	<p>{{ $image->file_name }}</p>
 </div>
 @endforeach
+
+
