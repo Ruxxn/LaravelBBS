5baa37fa3fe0fd3bf4f548193c97951bb7fffe90
Ruxon <wqtjdgus12@yahoo.co.jp>
2021-04-04 02:28:54 +0900

    aa

--- muryousichoulabo.xyz/public_html/BBS/resources/views/image_list.blade.php
+++ muryousichoulabo.xyz/public_html/BBS/resources/views/image_list.blade.php
@@ -3,14 +3,15 @@
 <a href="{{ route('upload_form') }}">Upload画面へ戻る</a>
 <hr />
 
+
 @foreach($images as $image)
 <div style="width: 18rem; float:left; margin: 16px;">
-	<img src="{{ Storage::url($image->file_path) }}" style="width:100%;"/>
-<?php
-	$a = Storage::url($image->file_path);
-	echo($a);
+      <img src="{{asset('storage/uploads/'.$image->file)}}" style="width:100%;"/>
+<?php   
+        $a = Storage::url($image->file_path);
+        echo($a);
 ?>
-	<p>{{ $image->file_name }}</p>
+        <p>{{ $image->file_name }}</p>
 </div>
 @endforeach
 
